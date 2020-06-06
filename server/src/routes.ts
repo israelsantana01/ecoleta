12f5e08493c3059377e95fb9a93c4b2a6486bc1c
israelsantana01 <israelofi2@gmail.com>
2020-06-06 20:51:45 -0300

    Add data validation in back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express, { request, response } from 'express';
+import { celebrate, Joi } from'celebrate';
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemController';
 import multer from 'multer';
@@ -14,6 +15,23 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', uploads.single('image') ,pointsController.create);
+routes.post(
+  '/points', 
+  uploads.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }), 
+  pointsController.create);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express, { request, response } from 'express';
 import path from 'path';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
 
